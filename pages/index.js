0ed41675065fe1cf4b71e2ac81ac037d016b3d09
Baddam Sahith Reddy <111670941+baddamsahithreddy@users.noreply.github.com>
2025-06-18 18:04:32 +0530

    Update and rename index.tsx to index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", paddingTop: "50px" }}>
+      <h1>LogicalInvestor Dashboard</h1>
+      <p>Welcome! Deployment is successful.</p>
+    </div>
+  );
+}
+

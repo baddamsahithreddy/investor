3e7b722793a2d61e729ad0bedbe398aa5f360b43
Baddam Sahith Reddy <111670941+baddamsahithreddy@users.noreply.github.com>
2025-06-20 00:01:31 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-module.exports = {
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEWS_API_KEY: process.env.NEWS_API_KEY,
-    STOCK_DATA_API: process.env.STOCK_DATA_API,
-    BROKER_API_KEY: process.env.BROKER_API_KEY,
-  },
+  // No other options unless needed
 };
+
+module.exports = nextConfig;
